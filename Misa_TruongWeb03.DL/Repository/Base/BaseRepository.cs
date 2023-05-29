624020d9c0c474512b5df97f2446913dc07357b7
TngoPC <tngo16062001@gmail.com>
2023-05-29 23:11:37 +0700

    sua loi delete

--- Misa_TruongWeb03.DL/Repository/Base/BaseRepository.cs
+++ Misa_TruongWeb03.DL/Repository/Base/BaseRepository.cs
@@ -198,7 +198,7 @@ namespace Misa_TruongWeb03.DL.Repository.Base
             try
             {
                 connection.Open();
-                var result = await connection.QueryFirstOrDefaultAsync<T>(storedProcedureName, parameters, commandType: CommandType.StoredProcedure);
+                var result = await connection.QueryFirstOrDefaultAsync<int>(storedProcedureName, parameters, commandType: CommandType.StoredProcedure);
                 var newResult = new BaseEntity
                 {
                     ErrorCode = StatusCodes.Status200OK,
